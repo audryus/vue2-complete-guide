9ea3bda4f9da5c42b06b880ee531d78bca740c35
Aubay Aml <amleite@aubay.com>
2020-04-28 14:31:18 +0100

    Section 13

---
+++ filtros-mixins/filtros-mixins-exercicios/src/Frutas.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{global}}</h1>
+    <ul>
+      <li v-for="f in frutas" :key="f">{{f}}</li>
+    </ul>
+    <input type="text" v-model="fruta" @keydown.enter.prevent="add">
+  </div>
+</template>
+
+<script>
+  import frutasMixin from './frutasMixin'
+  import usuarioMixin from './usuarioMixin'
+export default {
+  mixins: [frutasMixin, usuarioMixin],
+  created() {
+    console.log('Created - Frutas.vue');
+  }
+}
+</script>
+
+<style>
+
+</style>
